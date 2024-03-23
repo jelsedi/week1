e48274b6515a6eaab83edddd006eec82074c109c
jelsedi <jelsedi33@gmail.com>
2024-03-23 17:48:57 +0300

    math

--- challenge1.js
+++ challenge1.js
@@ -1,3 +1,20 @@
-function gradeGenerator(marks) {
-  
+function gradeGenerator(score) 
+{
+  if (score > 79) {
+  grade = "A";
+} 
+else if (score >= 60 && score <= 79) {
+  grade = "B";
 }
+ else if(score >=49 && score <= 59){
+  grade = "C";
+} 
+else if(score >= 40 && score <= 59){
+  grade = "D";
+} 
+else if(score < 40){
+  grade = "E";
+}
+return grade;
+}
+  
